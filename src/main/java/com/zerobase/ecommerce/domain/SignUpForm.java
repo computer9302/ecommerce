f48e93f56655888544f56ac44a55f662fe74cebf
computer9302 <computer9302@gmail.com>
2024-11-04 21:11:50 +0900

    [고객] 회원가입 개발

---
+++ src/main/java/com/zerobase/ecommerce/domain/SignUpForm.java
@@ -0,0 +1,14 @@
+package com.zerobase.ecommerce.domain;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class SignUpForm {
+    private String email;
+    private String name;
+    private String password;
+    private LocalDate birth;
+    private String phone;
+}
---
+++ src/main/java/com/zerobase/ecommerce/service/test/SignUpCustomerService.java
@@ -0,0 +1,18 @@
+package com.zerobase.ecommerce.service.test;
+
+import com.zerobase.ecommerce.domain.SignUpForm;
+import com.zerobase.ecommerce.domain.model.Customer;
+import com.zerobase.ecommerce.domain.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignUpCustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public Customer signUp(SignUpForm form){
+        return customerRepository.save(Customer.from(form));
+    }
+}
